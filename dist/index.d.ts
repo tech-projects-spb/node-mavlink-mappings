ca26d16846fe9ea0e5470df5fd819f6db8cc9673
Bogdanov Anton <51376798+Bogdanov-am@users.noreply.github.com>
2022-05-14 14:39:38 +0300

    update .gitignore

---
+++ dist/index.d.ts
@@ -0,0 +1,6 @@
+export * from './lib/types';
+export * from './lib/utils';
+export * from './lib/mavlink';
+export * from './lib/magic-numbers';
+export * as minimal from './lib/minimal';
+export * as common from './lib/common';
